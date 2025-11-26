8516613b87b710def605637dec75fa80eba712c7
Salahsr2005 <salah.serir@etu.univ-batna2.dz>
2025-11-27 00:20:27 +0100

    Implement Authentification with puter.js

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router"
-const NavBar = () => {
-  return (
-    <nav className="navbar">
-        <Link to="/">
-          <p className="texy-2xl font-bold text-gradient">Resumind Dz</p>
-        </Link>
-        <Link to="/upload" className="primary-button w-fit">
-          Upload Resume
-        </Link>
-    </nav>
-  )
-}
+import {Link} from "react-router";
 
-export default NavBar
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link to="/">
+                <p className="text-2xl font-bold text-gradient">RESUMIZER DZ</p>
+            </Link>
+            <Link to="/upload" className="primary-button w-fit">
+                Upload Resume
+            </Link>
+        </nav>
+    )
+}
+export default Navbar
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import NavBar from "~/components/NavBar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,9 +14,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const {auth} = usePuterStore();
+    const navigate = useNavigate();
+    useEffect(()=>{
+        if(!auth.isAuthenticated) navigate('auth?next=/');
+    },[auth.isAuthenticated]);
   return <>
-    <NavBar />
+    
     <main className="bg-[url('/images/bg-main.svg')] bg-cover ">
+        <NavBar />
         <section className="main-section" >
           <div className="page-heading py-16">
             <h1>
